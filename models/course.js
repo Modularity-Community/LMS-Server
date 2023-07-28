90684391e3acea200093981dd7dd9d23b0369038
azkifairuz <azkiajmal@gmail.com>
2023-07-29 01:02:33 +0700

    feat:create model update

--- models/course.js
+++ models/course.js
@@ -1,28 +1,48 @@
-const { query } = require("express")
-const conn = require("../connection")
-const {responseData,responseMessage} =  require('../utils/response-handler')
+const { query } = require("express");
+const conn = require("../connection");
+const { responseData, responseMessage } = require("../utils/response-handler");
 
-exports.insert = (response,statement,data) =>{
-    conn.query(statement,data,(err,rows,fields) => {
-      if (err) {
-        console.log(err.message);
-        return responseMessage(response,500,'internal error');
-      }
-
-      responseMessage(response,201,'Berhasil Insert Data')
-    })
-}
-
-exports.getAll = (response,statement,data) => {
-  conn.query(statement,data,(err,rows,fields)=>{
+exports.insert = (response, statement, data) => {
+  conn.query(statement, data, (err, rows, fields) => {
     if (err) {
       console.log(err.message);
-      return responseMessage(response,500,'internal error')
-    }
-    if (!fields?.affectedRows) {
-      return responseMessage(response,404,'data not found')
+      return responseMessage(response, 500, "internal error");
     }
 
-    return responseData(response,200,rows)
-  })
-}
+    responseMessage(response, 201, "Berhasil Insert Data");
+  });
+};
+
+exports.getAll = (response, statement, data) => {
+  conn.query(statement, data, (err, rows, fields) => {
+    if (err) {
+      console.log(err.message);
+      return responseMessage(response, 500, "internal error");
+    }
+    if (!fields?.affectedRows) {
+      return responseMessage(response, 404, "data not found");
+    }
+
+    return responseData(response, 200, rows);
+  });
+};
+
+exports.update = (response, searchStatement, updateStatement, id, data) => {
+  conn.query(searchStatement, id, (err, rows, field) => {
+    if (err) {
+      return responseMessage(response, 500, "internal error");
+    }
+
+    if (!rows?.length) {
+      return responseMessage(response, 404, "course tidak ditemukan");
+    }
+
+    conn.query(updateStatement, [data, id], (err, rows, field) => {
+      if (err) {
+        return responseMessage(response, 500, "internal error");
+      }
+
+      responseMessage(response, 200, "berhasil update course");
+    });
+  });
+};
