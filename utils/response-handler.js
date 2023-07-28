6325d21069e31cf03917394efb31802123ac688d
azkifairuz <azkiajmal@gmail.com>
2023-07-29 00:29:52 +0700

    feat:create responseMessage

--- utils/response-handler.js
+++ utils/response-handler.js
@@ -1,10 +1,21 @@
 const { response } = require("express");
 
-const responseData = function (response,statusCode,values) {
+const responseData = function (response, statusCode, values) {
   var data = {
     success: true,
-    data:values,
+    data: values,
   };
-  response.status(statusCode,values)
+  response.status(statusCode, values).json(data);
   response.end;
-}
+};
+
+const responseMessage = function (response, statusCode, message) {
+  var data = {
+    success: true,
+    message: message,
+  };
+  response.status(statusCode, values).json(data);
+  response.end;
+};
+
+module.exports = { responseData, responseMessage };
