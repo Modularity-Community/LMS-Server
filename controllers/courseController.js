ebb8df3d97fe268a939eeff97a1c4c34ba2d2d46
azkifairuz <azkiajmal@gmail.com>
2023-08-10 15:23:23 +0700

    refactor get course by id

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -3,7 +3,7 @@ const response = require("../utils/responseHandler");
 
 exports.getListCourse = (req, res) => {
   const sql =
-    "SELECT course.name as courseName, course.description,course.thumbnail,course.category_id,course.user_id, category.category,users.name FROM `course` INNER JOIN category ON course.category_id = category.id INNER JOIN users on course.user_id = users.id";
+    "SELECT course.id, course.name as courseName, course.description,course.thumbnail,course.category_id,course.user_id, category.category,users.name FROM `course` INNER JOIN category ON course.category_id = category.id INNER JOIN users on course.user_id = users.id";
   db.query(sql, (err, fields) => {
     if (err) {
       response.responseMessage(res, 500, "internal server error");
@@ -14,5 +14,24 @@ exports.getListCourse = (req, res) => {
       return;
     }
     response.responseData(res, 200, fields, "success");
+  });
+};
+
+exports.getListCourseById = (req, res) => {
+  const id = req.params.courseId;
+
+  const sql = `SELECT course.id, course.name as courseName, course.description,course.thumbnail,course.category_id,course.user_id, category.category,users.name FROM course INNER JOIN category ON course.category_id = category.id INNER JOIN users on course.user_id = users.id WHERE course.id = ${id}`;
+
+  db.query(sql, (err, rows, fields) => {
+    if (err) {
+      response.responseMessage(res, 500, err.message);
+      return;
+    }
+    if (rows.length === 0) {
+      response.responseMessage(res, 500, `data dengan id ${id} tidak ditemukan `);
+      return
+    }
+    response.responseData(res,200,rows,"success")
+    console.log(rows);
   });
 };
--- routes/courseRouter.js
+++ routes/courseRouter.js
@@ -1,8 +1,8 @@
-const courseContoller = require('../controllers/courseController')
+const courseContoller = require("../controllers/courseController");
 const express = require("express");
 const router = express.Router();
 
 router.get("/", courseContoller.getListCourse);
-
+router.get("/:courseId", courseContoller.getListCourseById);
 
 module.exports = router;
